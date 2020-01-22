27c4aebc4e23e5f84c5d3babda878554e9433873
Anton <antbjoerkman@gmail.com>
2020-01-22 13:05:19 +0100

    changes

---
+++ lab1/src/textproc/MultiWordCounter.java
@@ -0,0 +1,39 @@
+package textproc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MultiWordCounter implements TextProcessor{
+
+	Map<String, Integer> m = new HashMap<String, Integer>();
+	
+
+	public MultiWordCounter(String[] words) {
+		for(String w : words) {
+			m.put(w, 0);
+		}
+	}
+	
+	
+	@Override
+	public void process(String word) {
+		// TODO Auto-generated method stub
+		for(String s : m.keySet()) {
+			if(s.equals(word)) {
+				int i = m.get(s).intValue() + 1;
+				m.put(s, i);
+			}
+		}
+		
+	}
+
+	@Override
+	public void report() {
+		// TODO Auto-generated method stub
+		for(String s : m.keySet()) {
+			int i = m.get(s).intValue();
+			System.out.println(s + ": " + i);
+		}
+	}
+	
+}
